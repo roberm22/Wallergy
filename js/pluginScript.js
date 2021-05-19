c9c891fde48aae0df199ef7132487c1f9f2acae0
roberm22 <79532837+roberm22@users.noreply.github.com>
2021-05-19 13:53:35 +0200

    Add JavaScript

---
+++ js/pluginScript.js
@@ -0,0 +1,10 @@
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "fade",
+        randomize: true,
+        pauseOnHover: true,
+        controlNav: false,
+        directionNav: false,
+    });
+});
